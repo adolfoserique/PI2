4ec13525ac01217c69f8cd5393766fc2e1e204ae
adolfoserique <adolfoserique@gmail.com>
2019-06-24 17:27:24 -0300

    Update ATtiny85_Definitivo.ino

--- ATtiny85_Definitivo.ino
+++ ATtiny85_Definitivo.ino
@@ -1,22 +1,27 @@
 // SPI mode: CPOL = 0; CPHA = 1;
 
-#include <Wire.h> 
+
 #include <VL53L0X.h>
+#include <Wire.h> 
+
 
 const int CS_SPI = 4;                                  // set CS SPI to pin PB4
 const int SCLK_SPI = 3;                                // set SCLK SPI to pin PB3
 const int MISO_SPI = 1;                                // set MOSI SPI to pin PB1
-//const int SYNC_LASER = 5;                              // set Sync to pin PB5
+const int SYNC_LASER = 5;                              // set Sync to pin PB5
 int SCLK_STATE = 0;
 int CS_STATE = 0;
+int SYNC_STATE = 0;
 const byte numBits = 8;                                // set number of bits to send
 byte i = 0;
 byte state = 0;
 byte num = 0;
+byte num_last = 0;
 
 VL53L0X sensor;
 
 void setup() {
+
     Wire.begin();
     sensor.init();
     sensor.setTimeout(500);
@@ -26,64 +31,77 @@ void setup() {
 
     DDRB |= 0<<CS_SPI;                                 // set CS as input
     DDRB |= 0<<SCLK_SPI;                               // set SCLK as input
-    //DDRB |= 0<<SYNC_LASER;                             // set SYNC as input
     DDRB |= 1<<MISO_SPI;                               // set MISO as output
-    
+    pinMode(SYNC_LASER, INPUT);                       // set SYNC as input
+        
     PORTB &= ~(1 << MISO_SPI);                         // set MISO as LOW 
-  
+
     GIMSK = 0b00100000;                                // turns on pin change interrupts
-    PCMSK = 0b00011000;                                // turn on interrupts on pins PB3 and PB4
+    PCMSK = 0b00111000;                                // turn on interrupts on pins PB3, PB4 and PB5
+
 }
 
-void loop() {
 
-//     if (PINB & (1<<SYNC_LASER) != 0){
-//
-//        num = sensor.readRangeSingleMillimeters();     // read sensor
-//        //num = 167; // test
-//     
-//     }
-//     else {
-//        
-//        num = 0;
-//        
-//     }
+
+void loop() {
   
+
   }
+
+
 
       //-------SPI interupt routine (MODE 1)-------//
 
+
+
 ISR(PCINT0_vect)                                       // pin change interruption calls => 1°: CS goes high to low; 2°: SCLK goes low to high; 3°: SCLK goes high to low ...; 17°: SCLK goes high to low; 18°: CS goes low to high  
+
 {
+
     CS_STATE = (PINB & (1<<CS_SPI));                   // check CS state 
     SCLK_STATE = (PINB & (1<<SCLK_SPI));               // check SCLK state
-    
+    SYNC_STATE = digitalRead(SYNC_LASER);
+
     if (CS_STATE == 0){
-      
+
         if (SCLK_STATE == 0){
 
           state = bitRead(num, i-1);                   // save bit MSB first
-          i--;                                         // next bit
-        
+          i--;                                         // next bit       
+
         }
         else {
 
-          switch(state){
-            
+          switch(state){         
+
              case 0:
+
                 PORTB &= ~(1 << MISO_SPI);              // write bit 0
                 break;
+
              case 1:
+
                 PORTB |= (1 << MISO_SPI);               // write bit 1
-                break;
-                
+                break;          
+
           } 
        }        
     }
     else {
-      
+
       i=numBits;                                        // restart
       PORTB &= ~(1 << MISO_SPI);                        // disable pin
-      
-    }        
+
+      if (SYNC_STATE == 0){
+
+       num = num_last; // test
+
+     }
+     else {
+
+       num = sensor.readRangeSingleMillimeters();     // read sensor
+       num_last = num;
+
+     }  
+   }        
 }
