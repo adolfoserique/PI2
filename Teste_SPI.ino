b64e44b573f141eac67b27594c2f1504fcaacc32
adolfoserique <adolfoserique@gmail.com>
2019-06-24 17:28:56 -0300

    Update Teste_SPI.ino

--- Teste_SPI.ino
+++ Teste_SPI.ino
@@ -1,16 +1,23 @@
+const int SYNC_LASER = 7;
 const int CS = 8;
 const int CLK_SPI = 9;
-const int MEIO_PER_CLK = 1000;
-const int PAUSE = 500;
+const int MEIO_PER_CLK = 300;
+const int PAUSE = 1000;
 
 void setup() {
   pinMode(CS, OUTPUT);
   pinMode(CLK_SPI, OUTPUT);
+  pinMode(SYNC_LASER, OUTPUT);
   digitalWrite(CS, HIGH);
   digitalWrite(CLK_SPI, LOW);
+  digitalWrite(SYNC_LASER, HIGH);
 }
 
 void loop() {
+  digitalWrite(SYNC_LASER, HIGH);
+  delay(PAUSE);
+  digitalWrite(SYNC_LASER, LOW);
+  delayMicroseconds(10);
   digitalWrite(CS, HIGH);
   digitalWrite(CLK_SPI, LOW);
   delay(PAUSE);
@@ -50,4 +57,5 @@ void loop() {
   delay(MEIO_PER_CLK);
   delay(PAUSE);
   digitalWrite(CS, HIGH);
+  delayMicroseconds(10);
 }
